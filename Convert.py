98a8f2cc4221a90a75efc541b0890ca6a94c4eb3
saffaalvi <alvi118@uwindsor.ca>
2021-04-02 23:29:47 -0400

    Fix mistakes in Convert.py and disease.csv

--- Convert.py
+++ Convert.py
@@ -2,14 +2,12 @@ import numpy as np
 import pandas as pd
 
 diseases = pd.read_csv('disease.csv')
-
 columns = list(diseases.columns)[:-1]
-#print(columns)
 
 total_sym = len(columns)
 
 def to_valid_list(user_sym_list):
-    if type(user_sym)!=list:
+    if type(user_sym_list) != list:
         user_sym_list = to_list(user_sym_list)
     valid_list = []
     for i in range (0, total_sym-1):
@@ -20,15 +18,6 @@ def to_valid_list(user_sym_list):
     return(valid_list)
 
 def to_list(string):
-    user_sym_list = user_sym.split(",")
+    user_sym_list = string.split(",")
     user_sym_list = [x.strip(' ') for x in user_sym_list]
     return(user_sym_list)
-
-user_sym = 'cough, mass in breast, hypercapnia, orthopnea'
-print("ORIGINAL LIST: \n", user_sym, "\n")
-new_list = to_valid_list(user_sym)
-print("NEW LIST: \n", new_list)
-
-for i in range(0, len(new_list)-1):
-    if new_list[i] == 1:
-        print(columns[i])
